c0a63de7976b538cc03adfe17a1951b72919e86e
DeuterGraves <carolinetgraves@gmail.com>
2018-11-02 10:32:21 +0000

    added structure for obscuring api keys

---
+++ client/src/models/data_model.js
@@ -0,0 +1,30 @@
+const Key = require('../key.js');
+
+
+/* EXAMPLE OF HOW THE KEY.JS WAS ACCESSED
+const Request = require("../helpers/request.js")
+const PubSub = require("../helpers/pub_sub.js")
+const Key = require("../key.js");
+
+
+const TideTimes = function(){
+  this.data = null;
+};
+
+TideTimes.prototype.getData = function () {
+  const url = "http://localhost:3784/uktidalapi/api/V1/Stations/0226/TidalEvents";
+  const request = new Request(url);
+  request.get(Key)
+  .then((data) => {
+    this.data = data
+    console.log(data);
+  })
+
+
+};
+
+
+module.exports = TideTimes;
+*/
+
+module.exports =   ;
