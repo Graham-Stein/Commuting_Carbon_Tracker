267f377ab3604501931122334c713c47350e781a
Thomas Gracie <tgracie030@gmail.com>
2018-11-05 13:35:52 +0000

    Basic App JS sending useful event to calculator

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,17 @@
 const Calculator = require('./models/calculator.js');
+// const PubSub = require('./helpers/pub_sub.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JS Loaded');
-  // const calc = new Calculator();
+  const calc = new Calculator();
+  calc.bindEvents();
   const form = document.querySelector('#input-travel');
-  console.log('What is the form?', form);
+  // console.log('What is the form?', form);
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const event = evt;
+    // PubSub.publish('Form', event)
+    console.log('Event', event);
+  });
 
 });
