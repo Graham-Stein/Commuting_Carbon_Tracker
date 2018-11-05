400ac551cf347a40a4caa04860f32a3acf4bb70e
Thomas Gracie <tgracie030@gmail.com>
2018-11-05 13:17:48 +0000

    ESlintr up and running, adding app.js to send an event to the calculator

---
+++ client/src/app.js
@@ -0,0 +1,7 @@
+const Calculator = require('./models/calculator.js');
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JS Loaded');
+  // const calc = new Calculator();
+
+});
--- client/src/models/calculator.js
+++ client/src/models/calculator.js
@@ -1,18 +1,19 @@
-const PubSub = require("../helpers/pub_sub.js");
+const PubSub = require('../helpers/pub_sub.js');
 
-const Calculator = function(){
-  this.consts = [{"diesel", 0.218}, // add values here
-                 {"petrol", 0.286}, // gCO2e/km
-                 {"bus", 0.124},
-                 {"train", 0.05}]; // more figures on docs
+const Calculator = function() {
+  this.consts = [{'diesel': 0.218}, // add values here
+    {'petrol': 0.286}, // gCO2e/km
+    {'bus': 0.124},
+    {'hybrid': 0.118},
+    {'train': 0.05},
+    {'cycle': 0}]; // more figures on docs
 };
 
-// Calculator.prototype.bindEvents = function () {
-//   PubSub.subscribe('Form', (evt) => {
-//     this.data = evt.detail
-//   });
-// };
-
+Calculator.prototype.bindEvents = function() {
+  PubSub.subscribe('Form', (evt) => {
+    this.data = evt.detail;
+  });
+};
 
 
 module.exports = Calculator;
