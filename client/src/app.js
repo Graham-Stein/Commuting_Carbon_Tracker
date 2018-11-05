11f2efdf9b53434a92023502e14faaa5d93e7706
Thomas Gracie <tgracie030@gmail.com>
2018-11-05 13:27:23 +0000

    Updated html to use single form

--- client/src/app.js
+++ client/src/app.js
@@ -3,5 +3,7 @@ const Calculator = require('./models/calculator.js');
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JS Loaded');
   // const calc = new Calculator();
+  const form = document.querySelector('#input-travel');
+  console.log('What is the form?', form);
 
 });
