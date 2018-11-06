73b12f7729ad22df34c4cccb8c04791346bf91f1
DeuterGraves <carolinetgraves@gmail.com>
2018-11-06 11:21:43 +0000

    changed format of newCommuteData object

--- client/src/views/form_view.js
+++ client/src/views/form_view.js
@@ -25,11 +25,11 @@ FormView.prototype.createNewCommute = function(form) {
     singleTripDistance: form['single-trip-distance'].value,
     commutingDays: form['commuting-days'].value,
     singleTripsPerDay: form['single-trips-per-day'].value,
-    carDiesel: form['car-diesel'].value,
-    carPetrol: form['car-petrol'].value,
-    carHyrid: form['car-hybrid'].value,
-    bus: form['bus'].value,
-    cycle: form['cycle'].value,
+    modes: {carDiesel: form['car-diesel'].value,
+      carPetrol: form['car-petrol'].value,
+      carHyrid: form['car-hybrid'].value,
+      bus: form['bus'].value,
+      cycle: form['cycle'].value}
   };
   console.log('formview new item', newCommuteData);
   return newCommuteData;
--- client/src/views/result_view.js
+++ client/src/views/result_view.js
@@ -1,47 +1,49 @@
-const PubSub = require('./helpers/pub_sub.js');
+const PubSub = require('../helpers/pub_sub.js');
 
-const ResultView = function() {
-this.container = container;
+const ResultView = function(container) {
+  this.container = container;
 };
 
-ResultView.prototype.bindEvents = function () {
-    PubSub.subscribe('FormView:added-item', (evt) => {
+ResultView.prototype.bindEvents = function() {
+  PubSub.subscribe('FormView:added-item', (evt) => {
+    console.log('result view', evt);
     this.render(evt.details);
-  })
+  });
 };
+/*
+ResultView.prototype.render = function(form) {
+  const singleTripDistance = this.createTextElement('p', form.value);
+  this.container.appendChild(singleTripDistance);
 
-ResultView.prototype.render = function (form) {
-    const singleTripDistance = this.createTextElement('p', form.value)
-    this.container.appendChild(singleTripDistance)
+  const commutingDays =
+  this.createTextElement('p', form.value);
+  this.container.appendChild(commutingDays);
+  const singleTripsPerDay =
+  this.createTextElement('p', form.value);
+  this.container.appendChild(singleTripsPerDay);
 
-    const commutingDays =
-    this.createTextElement('p', form.value)
-    this.container.appendChild(commutingDays)
+  const carDiesel =
+  this.createTextElement('p', form.value);
+  this.container.appendChild(carDiesel);
 
-    const singleTripsPerDay =
-    this.createTextElement('p', form.value)
-    this.container.appendChild(singleTripsPerDay)
+  const carPetrol =
+  this.createTextElement('p', form.value);
+  this.container.appendChild(carPetrol);
 
-    const carDiesel =
-    this.createTextElement('p', form.value)
-    this.container.appendChild(carDiesel)
+  const carHyrid =
+  this.createTextElement('p', form.value);
+  this.container.appendChild(carHyrid);
 
-    const carPetrol =
-    this.createTextElement('p', form.value)
-    this.container.appendChild(carPetrol)
+  const bus =
+  this.createTextElement('p', form.value);
+  this.container.appendChild(bus);
 
-    const carHyrid =
-    this.createTextElement('p', form.value)
-    this.container.appendChild(carHyrid)
-
-    const bus =
-    this.createTextElement('p', form.value)
-    this.container.appendChild(bus)
-
-    const cycle =
-    this.createTextElement('p', form.value)
-    this.container.appendChild(cycle)
-}
+  const cycle =
+  this.createTextElement('p', form.value);
+  this.container.appendChild(cycle);
 };
+*/
 
-module.exports= ResultView;
+// };
+
+module.exports = ResultView;
