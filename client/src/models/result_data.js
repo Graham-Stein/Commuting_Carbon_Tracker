af54472881e86d0736a8c2ffc27a7f5c85b97adb
DeuterGraves <carolinetgraves@gmail.com>
2018-11-06 16:54:26 +0000

    result data calculations working makes object for highcharts

---
+++ client/src/models/result_data.js
@@ -0,0 +1,10 @@
+const AllResultData = function() {
+  this.carDiesel = null;
+  this.carPetrol = null;
+  this.carHybrid = null;
+  this.bus = null;
+  this.cycle = null;
+  this.userMix = null;
+};
+
+module.exports = AllResultData;
---
+++ client/src/views/result_view_play.js
@@ -0,0 +1,24 @@
+ResultView = require('./result_view.js');
+ResultData = require('../models/result_data.js');
+
+resultView = new ResultView();
+resultData = new ResultData();
+
+// populate the all outdata
+resultData.carDiesel = 1;
+resultData.carHybrid = 1;
+resultData.carPetrol = 2;
+resultData.bus = 3;
+resultData.cycle = 5;
+
+// create a dake of the outputUserData
+const outputUserData = {
+  carDiesel: 1,
+  carHyrid: 2,
+  carPetrol: 3,
+  bus: 1,
+  cycle: 1
+};
+
+//  call ResultView.populateUserCommuteData(outputUserData)
+resultView.populateUserCommuteData(outputUserData);
