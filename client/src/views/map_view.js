ad7ae0cb62aeb626e4ff666e695428bea4ac1e32
Graham-Stein <graham@guavoaviation.co.uk>
2018-11-07 14:30:37 +0000

    Add 2 maps & search

--- client/src/views/map_view.js
+++ client/src/views/map_view.js
@@ -1,29 +1,41 @@
 const MapKey = require('../key.js');
+const AutocompleteDirectionsHandler = require('./gmap.js');
 
-const MapView = function(formElement) {
-  this.element = formElement;
+const MapView = function(container, onInitComplete) {
+  this.container = container;
   this.googleMap = null;
+  this.onInitComplete = onInitComplete;
 };
 
 MapView.prototype.bindEvents = function() {
-  this.loadGoogleMapsAPI();
+  if (window.google) {
+    console.log('not inserting script again')
+    this.initMap();
+  }
+  else {
+    console.log('inserting script for gmaps API')
+    this.loadGoogleMapsAPI();
+  }
 };
 
 MapView.prototype.loadGoogleMapsAPI = function() {
   window.loadGoogleMapsApiCallback = () => {
     this.initMap();
+    this.onInitComplete();
   };
   const jsFile = document.createElement('script');
   jsFile.type = 'text/javascript';
-  jsFile.src = `https://maps.googleapis.com/maps/api/js?callback=loadGoogleMapsApiCallback&key=${MapKey}`;
+  jsFile.src = `https://maps.googleapis.com/maps/api/js?callback=loadGoogleMapsApiCallback&key=${MapKey}&libraries=places`;
   document.getElementsByTagName('head')[0].appendChild(jsFile);
 };
 
 MapView.prototype.initMap = function() {
-  this.googleMap = new google.maps.Map(document.getElementById('googleMap'), {
+  this.googleMap = new google.maps.Map(this.container, {
     center: {lat: 55.93715871276677, lng: -3.206435329645956},
     zoom: 10,
   });
+
+  new AutocompleteDirectionsHandler(this.googleMap);
 };
 
 module.exports = MapView;
